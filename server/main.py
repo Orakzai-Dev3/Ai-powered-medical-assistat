e356ac7aa933269713cca8b49a939c4cc10d61e3
Orakzai-Dev3 <arehman.dev3@gmail.com>
2026-07-03 09:07:12 +0500

    Added some thing

--- server/main.py
+++ server/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from middleware.exception_handler import exception_handler
 from routes.ask_question_route import Question_router
 from routes.Upload_pdf_route import Upload_pdf_router
-
+origins =[
+    'http://localhost:5173'
+]
 app = FastAPI(name='Medical Assistant',description='A medical assistant application that provides information and support to users based on their medical queries.',)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials = ['*'],
+    allow_origins=origins,
+    allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
 
